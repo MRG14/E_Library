8b8fb63bcc820093eaa868590e2eedd047ab31d3
MRG14 <ghilmanm731@gmail.com>
2026-04-06 19:57:16 +0700

    Changes to be committed: 	modified:   resources/views/books/index.blade.php

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -10,31 +10,31 @@
     </head>
     <body class="w-full bg-white flex flex-col">
         <!--Ini Header Start-->
-        <nav class="w-full h-20 flex flex-row  bg-gray-100 px-5 lg:px-10 items-center">
-            <div class="h-fit p-2 border">
-                E LIBRARY
-            </div>
+            <nav class="w-full h-20 flex flex-row  bg-gray-100 px-5 lg:px-10 items-center">
+                <div class="h-fit p-2 border">
+                    E LIBRARY
+                </div>
 
-            <ul class="hidden lg:flex flex-row gap-7.5 ms-auto items-center">
-                <li>
-                    <a href="/">Home</a>
-                </li>
-                <li>
-                   <a href="/books">Books</a>
-                </li>
-                <li>
-                    <a href="/categories">Categories</a>
-                </li>
-                <li>
-                    <button class="bg-blue-500 p-2">Login</button>
-                </li>
-            </ul>
+                <ul class="hidden lg:flex flex-row gap-7.5 ms-auto items-center">
+                    <li>
+                        <a href="/">Home</a>
+                    </li>
+                    <li>
+                       <a href="/books">Books</a>
+                    </li>
+                    <li>
+                        <a href="/categories">Categories</a>
+                    </li>
+                    <li>
+                        <button class="bg-blue-500 p-2">Login</button>
+                    </li>
+                </ul>
 
-            <div class="w-10 h-10 bg-gray-500 ms-auto inline lg:hidden">
+                <div class="w-10 h-10 bg-gray-500 ms-auto inline lg:hidden">
 
-            </div>
-        </nav>
-    <!--Header End-->
+                </div>
+            </nav>
+        <!--Header End-->
 
 
 
